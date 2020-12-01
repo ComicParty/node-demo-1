99345dabecc403a3006bbd49bc1cc993b56b5cd4
ComicParty <angerasura@icloud.com>
2020-12-01 20:40:04 +0800

    Create node.js

---
+++ node.js
@@ -0,0 +1,49 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(`<html doctype><head><link href="./css"></head><h1>h1</h1><body></body></html>`)
+    response.end()
+  } else if(path === '/x'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/css;charset=utf-8')
+    response.write(`body{color: red;}`)
+    response.end()
+  } else {
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(`你输入的路径不存在对应的内容`)
+    response.end()
+    
+ http://localhost:8888 得到一个 HTML 页面，页面里面有一个 h1 标签，并且页面会请求一个 style.css
+style.css 内容为 h1{color: red}
+访问其他未知路径一律提示「你访问的页面不存在」，并且状态码为 404
+  }
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
